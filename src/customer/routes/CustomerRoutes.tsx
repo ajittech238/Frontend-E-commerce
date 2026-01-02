a8c2557f78447387ca773a7ac0cb62d56944d750
Rahul Kumawat <rahulkumawat0992@gmail.com>
2026-01-02 11:48:32 +0530

    customer page

--- src/customer/routes/CustomerRoutes.tsx
+++ src/customer/routes/CustomerRoutes.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import CustomerDashboard from "../pages/CustomerDashboard";
+import CustomerOverview from "../pages/CustomerOverview";
 import CustomerProfile from "../pages/CustomerProfile";
 import CustomerOrders from "../pages/CustomerOrders";
 import CustomerWishlist from "../pages/CustomerWishlist";
 import CustomerAddresses from "../pages/CustomerAddresses";
 import ChangePassword from "../pages/ChangePassword";
+import CustomerNotifications from "../pages/CustomerNotifications";
+import CustomerReferEarn from "../pages/CustomerReferEarn";
+import CustomerRewards from "../pages/CustomerRewards";
+import CustomerQA from "../pages/CustomerQA";
 import ProtectedRoute from "./ProtectedRoute";
 
 const CustomerRoutes: React.FC = () => {
@@ -19,10 +24,15 @@ const CustomerRoutes: React.FC = () => {
           </ProtectedRoute>
         }
       >
-        <Route index element={<CustomerProfile />} />
+        <Route index element={<CustomerOverview />} />
+        <Route path="profile" element={<CustomerProfile />} />
         <Route path="orders" element={<CustomerOrders />} />
         <Route path="wishlist" element={<CustomerWishlist />} />
         <Route path="addresses" element={<CustomerAddresses />} />
+        <Route path="notifications" element={<CustomerNotifications />} />
+        <Route path="refer-earn" element={<CustomerReferEarn />} />
+        <Route path="rewards" element={<CustomerRewards />} />
+        <Route path="qa" element={<CustomerQA />} />
         <Route path="change-password" element={<ChangePassword />} />
       </Route>
     </Routes>
